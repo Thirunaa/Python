1f05006b4952ecf545c4cee253138ec6d92eb843
Thirunaa <messi.thiru04@gmail.com>
2020-05-27 23:57:17 +0530

    Add files via upload

---
+++ Tkinter/Test_Tkintermsgbox.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 16:03:55 2020
+
+@author: welcome
+"""
+
+from tkinter import *
+from tkinter import messagebox
+
+top = Tk()
+top.geometry("0x0")
+messagebox.showwarning("Info", "Wrong Input")
+top.mainloop()
