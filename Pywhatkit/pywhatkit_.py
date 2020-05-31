d9a5c2667da298e5c6fd14569826b392bc06ac57
Thirunaa <messi.thiru04@gmail.com>
2020-05-31 23:02:51 +0530

    Add files via upload

---
+++ Pywhatkit/pywhatkit_.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 22:27:52 2020
+
+@author: welcome
+"""
+
+import pywhatkit as kit
+
+kit.sendwhatmsg("+91 **********", "Your message", 22,30) #Opens whatsapp by 22:29 and sends the message at 22:30
+kit.playonyt("Rowdy Baby") #This navigates to youtube
+kit.info("MS Dhoni", lines=3)
